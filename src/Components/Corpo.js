fcbe8eb0557edb7cc63c16fa95a080fd51ab6bfe
Victoria <vic_andrade_@hotmail.com>
2022-11-16 20:09:01 -0300

    Creat components

---
+++ src/Components/Corpo.js
@@ -0,0 +1,15 @@
+import Stories from "./Stories";
+import Posts from "./Posts";
+import Sidebar from "./Sidebar";
+
+export default function Corpo() {
+  return (
+    <div className="corpo">
+      <div className="esquerda">
+        <Stories />
+        <Posts />
+      </div>
+      <Sidebar />
+    </div>
+  );
+}
